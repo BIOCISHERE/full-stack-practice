f336da05113b733b6242b22ec16cbc740e6794df
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-03-15 15:13:05 -0300

    navbar 1.0

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import fauxAtelierUrl from "../../img/faux_atelier_logo.png";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg fauxColor">
       <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">Faux Atelier</span>
+        <Link className="navbar-brand" to="/">
+          <img
+            className="rounded-circle"
+            src={fauxAtelierUrl}
+            alt="logo"
+            style={{ width: 50, height: 50 }}
+          ></img>
+          <span className="fs-3 fauxText m-1 ">Faux Atelier</span>
         </Link>
-        <div className="ml-auto">
-          <Link to="/demo">
-            <button className="btn btn-primary">
-              Check the Context in action
-            </button>
-          </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-link fauxText" to="/">
+              Mujer
+            </Link>
+            <Link className="nav-link fauxText" to="/">
+              Hombre
+            </Link>
+            <Link className="nav-link fauxText" to="/">
+              Calzado
+            </Link>
+            <Link className="nav-link fauxText" to="/">
+              SALE
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
