dbe1e046ee6e8fe9b211c978d76e860fc3b05f24
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-03-21 18:45:00 -0300

    footer 2.0

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,17 +1,73 @@
 import React, { Component } from "react";
+import { FaLocationArrow } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Footer = () => (
   <footer className="footer mt-auto py-3 fauxFooter">
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
         <div className="col-3">
-          <span>Locate us</span>
+          <span>
+            <span className="me-1">
+              <FaLocationArrow />
+            </span>
+            Locate us
+          </span>
         </div>
         <div className="col-3">
-          <span>Faux Atelier</span>
+          <span className="fw-bold">Faux Atelier</span>
+          <ul>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Stores
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Contact us
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Privacy policies
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="col-3">
-          <span>Information</span>
+          <span className="fw-bold">Information</span>
+          <ul>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Frequent questions
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Delivery
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Pick up in store
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Payment policies
+              </Link>
+            </li>
+            <li>
+              <Link className="text-decoration-none text-reset" to="/">
+                Terms and conditions
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="col-3">
           <span>Payments</span>
