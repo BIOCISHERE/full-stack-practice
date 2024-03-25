4ffa14e6ee8fef0a0a5872a96f733cdc3f0e2c64
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-03-25 17:09:52 -0300

    women's products 1.0

---
+++ src/front/js/pages/women's-products.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const WomensProducts = () => {
+  return (
+    <div>
+      <p>Women's Products</p>
+    </div>
+  );
+};
