705fd8074313d2395070a347595213c85d528a49
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-04-05 13:53:24 -0300

    men's products 3.0

--- src/front/js/pages/men's-products.js
+++ src/front/js/pages/men's-products.js
@@ -4,7 +4,66 @@ export const MensProducts = () => {
   return (
     <div className="container-fluid my-1">
       <div className="row">
-        <div className="col 2 border border-primary">men's products part1</div>
+        <div className="col 2 border border-primary">
+          <h4>Menswear</h4>
+          <div className="container-fluid my-1">
+            <h6>Type of garmnet</h6>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="tshirtCheck"
+                onClick={() => console.log("T-Shirt Working")}
+              />
+              <label className="form-check-label" htmlFor="tshirtCheck">
+                T-shirts
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="hoodieCheck"
+                onClick={() => console.log("Hoodie Working")}
+              />
+              <label className="form-check-label" htmlFor="hoodieCheck">
+                Hoodies/Sweaters
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="pantsCheck"
+                onClick={() => console.log("Pants Working")}
+              />
+              <label className="form-check-label" htmlFor="pantsCheck">
+                Pants
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="jeansCheck"
+                onClick={() => console.log("Jeans Working")}
+              />
+              <label className="form-check-label" htmlFor="jeansCheck">
+                Jeans
+              </label>
+            </div>
+          </div>
+          <div className="container-fluid my-1">
+            <h6>Accessories</h6>
+          </div>
+          <div className="container-fluid my-1">
+            <h6>Underwear</h6>
+          </div>
+        </div>
         <div className="col-10 border border-primary">
           men's products part 2
         </div>
