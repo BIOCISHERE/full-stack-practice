a01495bfc4f73e0a266374480e55a23aa704b25d
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-04-18 11:48:02 -0400

    footwear's products 1.0

---
+++ src/front/js/pages/footwear's-products.js
@@ -0,0 +1,14 @@
+import React, { useContext, useState } from "react";
+import tShirtUrl from "../../img/t-shirt.png";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import { FaRegStar, FaStar, FaRegHeart } from "react-icons/fa6";
+
+export const FootwearProducts = () => {
+  return (
+    <div className="container-fluid my-1">
+      <h1>Footwear's products</h1>
+      <h5>Page start</h5>
+    </div>
+  );
+};
