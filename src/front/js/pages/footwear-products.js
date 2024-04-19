c5e3a7be411776451b2921820840918879651f4e
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-04-19 17:39:22 -0400

    footwear products 2.0

--- src/front/js/pages/footwear-products.js
+++ src/front/js/pages/footwear-products.js
@@ -7,8 +7,63 @@ import { FaRegStar, FaStar, FaRegHeart } from "react-icons/fa6";
 export const FootwearProducts = () => {
   return (
     <div className="container-fluid my-1">
-      <h1>Footwear's products</h1>
-      <h5>Page start</h5>
+      <div className="row">
+        <div className="col-2 border border-primary">
+          <h4>Footwear</h4>
+          <div className="container-fluid my-1">
+            <h6>Type of footwear</h6>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="urbanCheck"
+                onClick={() => console.log("Urban Working")}
+              />
+              <label className="form-check-label" htmlFor="urbanCheck">
+                Urban Sneakers
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="sportCheck"
+                onClick={() => console.log("Sport Working")}
+              />
+              <label className="form-check-label" htmlFor="sportCheck">
+                Sport Sneakers
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="shoesCheck"
+                onClick={() => console.log("Shoes Working")}
+              />
+              <label className="form-check-label" htmlFor="shoesCheck">
+                Shoes
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="sandalsCheck"
+                onClick={() => console.log("Sandals Working")}
+              />
+              <label className="form-check-label" htmlFor="sandalsCheck">
+                Sandals
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="col-10 border border-primary">Footwear part 2</div>
+      </div>
     </div>
   );
 };
