c84fc9e02b968625788f6724fbf6c609b5345396
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-04-23 11:11:39 -0400

    footwear products 4.0

--- src/front/js/pages/footwear-products.js
+++ src/front/js/pages/footwear-products.js
@@ -12,6 +12,16 @@ export const FootwearProducts = () => {
   const [isShoe, setIsShoe] = useState(false);
   const [isSandals, setIsSandals] = useState(false);
 
+  const test = [
+    {
+      id: 1,
+      name: "Urban sneakers",
+      cost: 10000,
+      rating: 4,
+      ratingVotes: 200,
+    },
+  ];
+
   const turnClass = (info) => {
     if (info.category == 17) {
       return isUrban ? "col" : "d-none";
@@ -80,7 +90,40 @@ export const FootwearProducts = () => {
             </div>
           </div>
         </div>
-        <div className="col-10 border border-primary">Footwear part 2</div>
+        <div className="col-10 border border-primary">
+          <div className="container-fluid">
+            <div className="row row-cols-xm-1 row-cols-sm-2 row-cols-md-4 g-4">
+              <div className="col">
+                <div className="card my-1">
+                  <img
+                    className="card-image-top img-fluid"
+                    src={tShirtUrl}
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{test[0].name}</h5>
+                    <div className="d-flex">
+                      <span className="card-text me-auto">${test[0].cost}</span>
+                      <span className="card-text fauxLetters fs-5">
+                        <FaRegHeart />
+                      </span>
+                    </div>
+                    <span className="card-text fauxLetters">
+                      {actions.turnRating(test[0].rating)}
+                    </span>
+                    <span className="card-text ms-1">
+                      ({test[0].ratingVotes})
+                    </span>{" "}
+                    <br />
+                    <Link to="/" className="btn btn-primary mt-1">
+                      Go somewhere
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
