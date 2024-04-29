5dbef2990b4bb55ee4d56c51036731a8ae12703c
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-04-29 16:53:25 -0400

    log-in 2.0

--- src/front/js/pages/log-in.js
+++ src/front/js/pages/log-in.js
@@ -8,7 +8,50 @@ export const LogIn = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <h1>Log In</h1>
+        <div className="col-6 border border-primary mx-auto my-4">
+          <h1 className="text-center">TEST</h1>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <div className="container-fluid text-center mx-auto">
+              <button type="button" className="btn btn-primary mb-2">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
