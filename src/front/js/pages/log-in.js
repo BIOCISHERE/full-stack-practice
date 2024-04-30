e7c2705cda6fa05cf03e48bfe5c1eac246acaadd
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-04-30 15:13:52 -0400

    log-in 3.0

--- src/front/js/pages/log-in.js
+++ src/front/js/pages/log-in.js
@@ -5,44 +5,69 @@ import { Context } from "../store/appContext";
 import { FaRegStar, FaStar, FaRegHeart } from "react-icons/fa6";
 
 export const LogIn = () => {
+  const { store, actions } = useContext(Context);
+
+  const [isEmail, setIsEmail] = useState("");
+  const [isPassword, setIsPassword] = useState("");
+  const [isConfirm, setIsConfirm] = useState(""); //this the confirm password state
+  const [isShow, setIsShow] = useState(false); // we will use this for the check to hide or show the password
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-6 border border-primary mx-auto my-4">
-          <h1 className="text-center">TEST</h1>
+          <h1 className="text-center">Log-in</h1>
           <form>
             <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">
+              <label htmlFor="emailInput" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
                 className="form-control"
-                id="exampleInputEmail1"
+                id="emailInput"
                 aria-describedby="emailHelp"
+                placeholder="example@gmail.com"
+                value={isEmail}
+                onChange={(e) => setIsEmail(e.target.value)}
               />
               <div id="emailHelp" className="form-text">
                 We'll never share your email with anyone else.
               </div>
             </div>
             <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">
+              <label htmlFor="passwordInput" className="form-label">
                 Password
               </label>
               <input
                 type="password"
                 className="form-control"
-                id="exampleInputPassword1"
+                id="passwordInput"
+                value={isPassword}
+                onChange={(e) => setIsPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="confirmInput" className="form-label">
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmInput"
+                value={isConfirm}
+                onChange={(e) => setIsConfirm(e.target.value)}
               />
             </div>
             <div className="mb-3 form-check">
               <input
                 type="checkbox"
                 className="form-check-input"
-                id="exampleCheck1"
+                id="checkInput"
+                onClick={() => setIsShow(!isShow)}
               />
-              <label className="form-check-label" htmlFor="exampleCheck1">
-                Check me out
+              <label className="form-check-label" htmlFor="checkInput">
+                Show password
               </label>
             </div>
             <div className="container-fluid text-center mx-auto">
