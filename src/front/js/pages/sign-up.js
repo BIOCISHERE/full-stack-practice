a7ac5b7ba14967aec741923167b91e468da9e8cb
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-05-02 17:56:30 -0400

    sign-up 1.0

---
+++ src/front/js/pages/sign-up.js
@@ -0,0 +1,14 @@
+import React, { useContext, useState } from "react";
+import tShirtUrl from "../../img/t-shirt.png";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const SignUp = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <h1>Sign up</h1>
+      </div>
+    </div>
+  );
+};
