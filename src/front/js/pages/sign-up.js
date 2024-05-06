296372d40058fd9ae438aaa3a788e3b0cc1c2662
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-05-06 15:57:37 -0400

    sign-up 2.0

--- src/front/js/pages/sign-up.js
+++ src/front/js/pages/sign-up.js
@@ -7,7 +7,60 @@ export const SignUp = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <h1>Sign up</h1>
+        <div className="col-6 border border-primary mx-auto my-4">
+          <h1 className="text-center">Sign-up</h1>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="emailInput" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="emailInput"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordInput" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="passwordInput"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="confirmInput" className="form-label">
+                Confirm password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="confirmInput"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="checkboxInput"
+              />
+              <label className="form-check-label" htmlFor="checkboxInput">
+                Check me out
+              </label>
+            </div>
+            <div className="container-fluid text-center mx-auto">
+              <button type="button" className="btn btn-primary mb-2">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
