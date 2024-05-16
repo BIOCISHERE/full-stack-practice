0cbffcbd49b9b2158528fb04c78aaff8a346b999
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-05-16 18:25:58 -0400

    product-view 6.0

--- src/front/js/pages/product-view.js
+++ src/front/js/pages/product-view.js
@@ -8,16 +8,58 @@ export const ProductView = () => {
   const { store, actions } = useContext(Context);
   const { id } = useParams();
 
+  const [isSize, setIsSize] = useState("S");
+  const [isAmount, setIsAmount] = useState("1");
+
+  const manageLinks = () => {
+    if (store.fullResponse[id - 1].productFor == "Women") {
+      return (
+        <span>
+          <Link to="/" className="mx-1">
+            Home
+          </Link>
+          {">"}
+          <Link to="/women-products" className="mx-1">
+            Women
+          </Link>
+          {">"}
+          <Link to="#" className="mx-1">
+            {store.fullResponse[id - 1].name}
+          </Link>
+        </span>
+      );
+    } else if (store.fullResponse[id - 1].productFor == "Men") {
+      return (
+        <span>
+          <Link to="/" className="mx-1">
+            Home
+          </Link>
+          {">"}
+          <Link to="/men-products" className="mx-1">
+            Men
+          </Link>
+          {">"}
+          <Link to="#" className="mx-1">
+            {store.fullResponse[id - 1].name}
+          </Link>
+        </span>
+      );
+    } else {
+      return <span>Fail</span>;
+    }
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-11 mx-auto">
           <div className="container-fluid">
             <div className="row">
-              <div className="col-8 border border-primary text-center">
+              <div className="container-fluid my-2">{manageLinks()}</div>
+              <div className="col-8 text-center">
                 <img className="img-fluid" src={tShirtUrl} alt="..." />
               </div>
-              <div className="col-4 border border-primary">
+              <div className="col-4 border border-dark-subtle rounded">
                 <div className="d-flex mt-2">
                   <span className="fs-2 me-auto text-break">
                     {store.fullResponse[id - 1].name}
@@ -49,7 +91,7 @@ export const ProductView = () => {
                     className="form-select border border-dark fauxBorder w-50"
                     aria-label="Select your size"
                     name="selectedSize"
-                    onChange={(e) => console.log(e.target.value)}
+                    onChange={(e) => setIsSize(e.target.value)}
                   >
                     <option defaultValue>S</option>
                     <option value="M">M</option>
@@ -57,14 +99,14 @@ export const ProductView = () => {
                     <option value="XL">XL</option>
                   </select>
                 </div>
-                <div className="container-fluid mt-2">
+                <div className="container-fluid mt-2 mb-4">
                   <label htmlFor="selectAmount">Select amount:</label>
                   <select
                     id="selectAmount"
                     className="form-select border border-dark fauxBorder w-50"
                     aria-label="Select how many"
                     name="selectedAmount"
-                    onChange={(e) => console.log(e.target.value)}
+                    onChange={(e) => setIsAmount(e.target.value)}
                   >
                     <option defaultValue>1</option>
                     <option value="2">2</option>
@@ -82,7 +124,7 @@ export const ProductView = () => {
                   <button
                     type="button"
                     className="btn btn-dark fauxColor w-100"
-                    onClick={() => console.log("test")}
+                    onClick={() => console.log(isSize, isAmount)}
                   >
                     Add to cart
                   </button>
