c54da0fc037a2c9574996f395b98e8bf16fad298
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-05-17 16:55:59 -0400

    product-view 7.0

--- src/front/js/pages/product-view.js
+++ src/front/js/pages/product-view.js
@@ -2,14 +2,17 @@ import React, { useContext, useState } from "react";
 import tShirtUrl from "../../img/t-shirt.png";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
-import { FaRegStar, FaStar, FaRegHeart } from "react-icons/fa6";
+import { FaRegStar, FaStar, FaRegHeart, FaHeart } from "react-icons/fa6";
 
 export const ProductView = () => {
   const { store, actions } = useContext(Context);
   const { id } = useParams();
 
   const [isSize, setIsSize] = useState("S");
+  const [isFootwearSize, setIsFootwearSize] = useState("30");
   const [isAmount, setIsAmount] = useState("1");
+
+  const [isFav, setIsFav] = useState(false);
 
   const manageLinks = () => {
     if (store.fullResponse[id - 1].productFor == "Women") {
@@ -44,8 +47,90 @@ export const ProductView = () => {
           </Link>
         </span>
       );
+    } else if (store.fullResponse[id - 1].productFor == "Footwear") {
+      <span>
+        <Link to="/" className="mx-1">
+          Home
+        </Link>
+        {">"}
+        <Link to="/footwear-products" className="mx-1">
+          Footwear
+        </Link>
+        {">"}
+        <Link to="#" className="mx-1">
+          {store.fullResponse[id - 1].name}
+        </Link>
+      </span>;
     } else {
       return <span>Fail</span>;
+    }
+  };
+
+  const manageSelect = () => {
+    if (store.fullResponse[id - 1].productFor == "Footwear") {
+      return (
+        <>
+          <label htmlFor="selectFootwear">Select size:</label>
+          <select
+            id="selectFootwear"
+            className="form-select border border-dark fauxBorder w-50"
+            aria-label="Select your size"
+            name="selectedSize"
+            onChange={(e) => setIsFootwearSize(e.target.value)}
+          >
+            <option defaultValue>30</option>
+            <option value="31">31</option>
+            <option value="32">32</option>
+            <option value="33">33</option>
+            <option value="34">34</option>
+            <option value="35">35</option>
+            <option value="36">36</option>
+            <option value="37">37</option>
+            <option value="38">38</option>
+            <option value="39">39</option>
+            <option value="40">40</option>
+            <option value="41">41</option>
+            <option value="42">42</option>
+            <option value="43">43</option>
+            <option value="44">44</option>
+            <option value="45">45</option>
+          </select>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <label htmlFor="selectSize">Select size:</label>
+          <select
+            id="selectSize"
+            className="form-select border border-dark fauxBorder w-50"
+            aria-label="Select your size"
+            name="selectedSize"
+            onChange={(e) => setIsSize(e.target.value)}
+          >
+            <option defaultValue>S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+          </select>
+        </>
+      );
+    }
+  };
+
+  const manageButton = () => {
+    if (store.fullResponse[id - 1].productFor == "Footwear") {
+      return console.log(isFootwearSize, isAmount);
+    } else {
+      return console.log(isSize, isAmount);
+    }
+  };
+
+  const manageFav = () => {
+    if (isFav) {
+      return <FaHeart type="button" onClick={() => setIsFav(!isFav)} />;
+    } else {
+      return <FaRegHeart type="button" onClick={() => setIsFav(!isFav)} />;
     }
   };
 
@@ -53,9 +138,9 @@ export const ProductView = () => {
     <div className="container-fluid">
       <div className="row">
         <div className="col-11 mx-auto">
-          <div className="container-fluid">
+          <div className="container-fluid my-2">{manageLinks()}</div>
+          <div className="container-fluid my-4">
             <div className="row">
-              <div className="container-fluid my-2">{manageLinks()}</div>
               <div className="col-8 text-center">
                 <img className="img-fluid" src={tShirtUrl} alt="..." />
               </div>
@@ -64,9 +149,7 @@ export const ProductView = () => {
                   <span className="fs-2 me-auto text-break">
                     {store.fullResponse[id - 1].name}
                   </span>
-                  <span className="fauxLetters fs-2">
-                    <FaRegHeart />
-                  </span>
+                  <span className="fauxLetters fs-2">{manageFav()}</span>
                 </div>
                 <div className="d-flex">
                   <span className="mx-1 fs-5">
@@ -84,21 +167,7 @@ export const ProductView = () => {
                     $ {store.fullResponse[id - 1].cost}
                   </span>
                 </div>
-                <div className="container-fluid mt-2">
-                  <label htmlFor="selectSize">Select size:</label>
-                  <select
-                    id="selectSize"
-                    className="form-select border border-dark fauxBorder w-50"
-                    aria-label="Select your size"
-                    name="selectedSize"
-                    onChange={(e) => setIsSize(e.target.value)}
-                  >
-                    <option defaultValue>S</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                  </select>
-                </div>
+                <div className="container-fluid mt-2">{manageSelect()}</div>
                 <div className="container-fluid mt-2 mb-4">
                   <label htmlFor="selectAmount">Select amount:</label>
                   <select
@@ -124,7 +193,7 @@ export const ProductView = () => {
                   <button
                     type="button"
                     className="btn btn-dark fauxColor w-100"
-                    onClick={() => console.log(isSize, isAmount)}
+                    onClick={() => manageButton()}
                   >
                     Add to cart
                   </button>
