8b143937b7aa23de407c05f0c3ce5c0ced12030b
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-07-23 14:35:49 -0400

    user-shippment 3.0

--- src/front/js/pages/user-shipment.js
+++ src/front/js/pages/user-shipment.js
@@ -12,8 +12,41 @@ export const UserShipment = () => {
       </div>
       <div className="container-fluid">
         <div className="row">
-          <div className="col">
-            <h1>UserShipment</h1>
+          <div className="col-8 border border-dark rounded mx-auto">
+            <div className="container-fluid">
+              <form>
+                <div className="d-flex">
+                  <div className="my-3 me-auto">
+                    <label htmlFor="firtsNameInput" className="form-label">
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control border border-dark-subtle"
+                      id="firtsNameInput"
+                      style={{ width: "22rem" }}
+                    />
+                  </div>
+                  <div className="my-3">
+                    <label htmlFor="lastNameInput" className="form-label">
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control border border-dark-subtle"
+                      id="lastNameInput"
+                      style={{ width: "22rem" }}
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <h1>adress</h1>
+            <h1>appartment (optional)</h1>
+            <h1>city</h1>
+            <h1>state/region</h1>
+            <h1>postal code (optional)</h1>
+            <h1>additional message</h1>
           </div>
         </div>
       </div>
