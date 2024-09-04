e26b9415574f493a4f778bf3db4cf60d92798a3b
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-09-04 13:57:52 -0400

    payment 3.0

--- src/front/js/pages/payment.js
+++ src/front/js/pages/payment.js
@@ -5,7 +5,26 @@ export const Payment = () => {
     <div className="container-fluid">
       <div className="row">
         <div className="col">
-          <h1>payment</h1>
+          <h1>Contact Info</h1>
+          <div className="row">
+            <div className="col">
+              <span>Profile picture</span>
+            </div>
+            <div className="col">
+              <span>Profile name</span> <br />
+              <span>Profile mail</span> <br />
+              <span>Profile phone</span>
+            </div>
+          </div>
+        </div>
+        <div className="col bg-body-secondary">
+          <h1>Shopping cart</h1>
+          <div className="row">
+            <div className="col-3">Product</div>
+            <div className="col-3">Price</div>
+            <div className="col-3">Amount</div>
+            <div className="col-3">Total</div>
+          </div>
         </div>
       </div>
     </div>
