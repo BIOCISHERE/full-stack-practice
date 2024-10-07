4a8067df5971ded0b6f3b455858253969504ee5c
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-10-07 16:25:16 -0300

    cartTotal 3.0, considering to change from class component to functional component

--- src/front/js/component/cartTotal.js
+++ src/front/js/component/cartTotal.js
@@ -8,9 +8,59 @@ class CartTotal extends Component {
   constructor() {
     super();
     this.state = {
-      test: "test",
+      isShowShipping: false,
+      isShipping: "1",
+      isReturnShipping: 10,
+      isShippingRegion: "North america",
+      isCartTotal: 0,
     };
   }
+
+  returnShipping = () => {
+    if (this.state.isShipping == 1) {
+      this.setState({ isReturnShipping: 10 });
+    } else if (this.state.isShipping == 2) {
+      this.setState({ isReturnShipping: 15 });
+    } else if (this.state.isShipping == 3) {
+      this.setState({ isReturnShipping: 20 });
+    } else if (this.state.isShipping == 4) {
+      this.setState({ isReturnShipping: 25 });
+    } else if (this.state.isShipping == 5) {
+      this.setState({ isReturnShipping: 15 });
+    } else if (this.state.isShipping == 6) {
+      this.setState({ isReturnShipping: 20 });
+    } else {
+      this.setState({ isReturnShipping: 15 });
+    }
+  };
+
+  returnShippingRegion = () => {
+    if (this.state.isShipping == 1) {
+      this.setState({ isShippingRegion: "North america" });
+    } else if (this.state.isShipping == 2) {
+      this.setState({ isShippingRegion: "South america" });
+    } else if (this.state.isShipping == 3) {
+      this.setState({ isShippingRegion: "Antartica" });
+    } else if (this.state.isShipping == 4) {
+      this.setState({ isShippingRegion: "Africa" });
+    } else if (this.state.isShipping == 5) {
+      this.setState({ isShippingRegion: "Europe" });
+    } else if (this.state.isShipping == 6) {
+      this.setState({ isShippingRegion: "Asia" });
+    } else {
+      this.setState({ isShippingRegion: "Australia" });
+    }
+  };
+
+  returnShoppingCartTotal = () => {
+    if (actions.getTotalProductInCart() > 0) {
+      this.setState({
+        isCartTotal: actions.getTotalCartCost() + this.state.isReturnShipping,
+      });
+    } else {
+      this.setState({ isCartTotal: 0 });
+    }
+  };
 
   render() {
     const { store, actions } = this.context;
@@ -34,11 +84,66 @@ class CartTotal extends Component {
           <div className="d-flex my-1">
             <span className="me-auto fs-5">
               Shipping
-              <span className="fs-6">({returnShippingRegion()})</span>
+              <span className="fs-6">({this.state.isShippingRegion})</span>
             </span>
-            <span className="fs-5 fw-bold">${returnShipping()}</span>
+            <span className="fs-5 fw-bold">${this.state.isReturnShipping}</span>
           </div>
-          <div>{/* Continue here */}</div>
+          <div className="form-check my-1">
+            <input
+              className="form-check-input border border-dark"
+              type="checkbox"
+              id="changeShippingInput"
+              onChange={() =>
+                this.setState({ isShowShipping: !this.state.isShowShipping })
+              }
+            />
+            <label
+              className="form-check-label fauxLetters"
+              htmlFor="changeShippingInput"
+            >
+              Change shipping region
+            </label>
+          </div>
+          <div
+            className={this.state.isShowShipping ? "container-fluid" : "d-none"}
+          >
+            <label htmlFor="selectShippingRegion" className="me-1">
+              Select region:
+            </label>
+            <select
+              id="selectShippingRegion"
+              onChange={(e) => (
+                this.setState({ isShipping: e.target.value }),
+                actions.changeShippingRegion(e.target.value),
+                this.returnShipping,
+                this.returnShippingRegion
+              )}
+            >
+              <option value={"1"}>North america</option>
+              <option value={"2"}>South america</option>
+              <option value={"3"}>Antartica</option>
+              <option value={"4"}>Africa</option>
+              <option value={"5"}>Europe</option>
+              <option value={"6"}>Asia</option>
+              <option value={"7"}>Australia</option>
+            </select>
+          </div>
+          <div className="d-flex mt-1 mb-3">
+            <span className="me-auto fs-5">Total</span>
+            <span className="fs-5 fw-bold">${this.state.isCartTotal}</span>
+          </div>
+          <div className="container-fluid my-1 mb-2">
+            <button
+              type="button"
+              className="btn btn-dark fauxColor w-100"
+              onClick={() => (
+                console.log("isShipping", this.state.isShipping),
+                console.log("flux", store.shipping)
+              )}
+            >
+              Buy
+            </button>
+          </div>
         </div>
       </>
     );
